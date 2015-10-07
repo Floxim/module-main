3cf2e65a923fb1010d3cc310dba6e6d637d7725a
Ilya <dubr.cola@gmail.com>
2015-10-07 06:39:09 +0300

    message templates

--- MessageTemplate/Entity.php
+++ MessageTemplate/Entity.php
@@ -2,7 +2,32 @@
 
 namespace Floxim\Main\MessageTemplate;
 
+use Floxim\Floxim\System\Fx;
+
 class Entity extends \Floxim\Main\Content\Entity
 {
-
+    public function process($data) {
+        $com_content_id = fx::component('floxim.main.content')->get('id');
+        $res = array();
+        foreach ($this->getFields() as $f) {
+            $type = $f->getTypeKeyword();
+            
+            $field_keyword = $f['keyword'];
+            
+            if (!in_array($type, array('string', 'text'))) {
+                continue;
+            }
+            if ($f['component_id'] === $com_content_id) {
+                continue;
+            }
+            if ($field_keyword === 'keyword') {
+                continue;
+            }
+            $prop_tpl = $this[$field_keyword];
+            $tpl_obj = fx::template()->virtual($prop_tpl);
+            $tpl_obj->isAdmin(false);
+            $res[$field_keyword] = $tpl_obj->render($data);
+        }
+        return $res;
+    }
 }
--- MessageTemplate/Finder.php
+++ MessageTemplate/Finder.php
@@ -4,5 +4,11 @@ namespace Floxim\Main\MessageTemplate;
 
 class Finder extends \Floxim\Main\Content\Finder
 {
-
+    public function getById($id) {
+        if (!is_numeric($id) && strlen($id) > 0) {
+            $this->where('keyword', $id);
+            return $this->one();
+        }
+        return parent::getById($id);
+    }
 }
