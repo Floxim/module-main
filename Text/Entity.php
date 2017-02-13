67f75ba7f41f7d5fe09b07c2961acbdad1c00b2f
dubr <dubr.cola@gmail.com>
2017-02-13 23:37:41 +0300

    default text box

--- Text/Entity.php
+++ Text/Entity.php
@@ -3,6 +3,12 @@ namespace Floxim\Main\Text;
 
 class Entity extends \Floxim\Main\Content\Entity
 {
-
-
+    public function getDefaultBoxFields()
+    {
+        return array(
+            array(
+                array('keyword' => 'text')
+            )
+        );
+    }
 }
